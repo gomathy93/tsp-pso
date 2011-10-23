555c44ff1f2a3fe03481e22753bfb1178e2f03da
maciek.muszkowski@gmail.com <maciek.muszkowski@gmail.com@64a65b12-7acc-f961-7171-c0332ff978a3>
2011-10-23 11:30:36 +0000

    Usuniete pare niepotrzebnych rzeczy.

--- trunk/Particle.h
+++ trunk/Particle.h
@@ -8,7 +8,13 @@ namespace PSO
 	class Particle
 	{
 		/** Roj do ktorego przynalezy czastka */
-		Swarm*	swarm;
+		Swarm*			swarm;
+		/** Najlepsze znalezione rozwiazanie (przez ta czastke) */
+		vector<float>	best;
+		/** Wspolczynnik bezwladnosci */
+		static const float OMEGA;
+		/** Stale przyspieszenia */
+		static const float C1, C2;
 	public:
 		/** Tworzy czastke przynalezaca do roju swarm */
 		Particle(Swarm* swarm)
@@ -16,8 +22,17 @@ namespace PSO
 			this->swarm = swarm;
 		}
 		/** Pozycja czastki */
-		vector<double>	position;
+		vector<float>	position;
 		/** Predkosc czastki */
-		vector<double>	velocity;
+		vector<float>	speed;
+		/** Aktualizuje predkosc i polozenie w kolejnej iteracji */
+		void update()
+		{
+			vector<float> bestGlobal& = swarm->best;
+			for(int i=0; i<speed.size(); i++)
+				speed[i] = OMEGA*speed[i] + C1*frand(0.0f, best[i] - position[i]) + C2*frand(0.0f, bestGlobal[i] - position[i]);
+			for(int i=0; i<position.size(); i++)
+				position[i] += speed[i];
+		}
 	};
 }; // namespace PSO
--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,7 +1,12 @@
 #include "stdafx.h"
 
+#include "Graph.h"
+using namespace PSO;
+
 int main(int argc, char* argv[])
 {
+	Graph g(30, 435, 10.0f); // pelny
+
 	return 0;
 }
 
